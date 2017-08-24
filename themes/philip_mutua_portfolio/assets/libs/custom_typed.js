e0a6de67e9435b886b0eb51bcede558e627955f4
pmutua <pmutua@live.com>
2017-08-24 20:17:17 +0300

    update index add typed js

---
+++ themes/philip_mutua_portfolio/assets/libs/custom_typed.js
@@ -0,0 +1,18 @@
+  var typed6 = new Typed('.element', {
+      strings: ['npm install^1000\n `installing components...` ^1000\n `Fetching from source...`'],
+      typeSpeed: 40,
+      backSpeed: 0,
+      loop: true
+  });
+
+
+  var typed2 = new Typed('.element2', {
+      strings: ['<span style="color:white !important">HI!</span>', 'I\nm', '<span>Philip Mutua</span>'],
+      typeSpeed: 0,
+      backSpeed: 0,
+      fadeOut: true,
+      loop: true //change loop text
+  });
+
+
+  //   HI!I 'm <span>Philip Mutua</span>
