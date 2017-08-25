343a174870ae193cb9cc9c9ec8dd90246f56c320
pmutua <pmutua@live.com>
2017-08-25 11:22:31 +0300

    update index.html more styling done + added mac look alike in about and did styling

--- themes/philip_mutua_portfolio/assets/libs/custom_typed.js
+++ themes/philip_mutua_portfolio/assets/libs/custom_typed.js
@@ -1,5 +1,6 @@
   var typed6 = new Typed('.element', {
-      strings: ['npm install^1000\n `installing components...` ^1000\n `Fetching from source...`'],
+      strings: [' Hi,My name is Philip Mutua I am a creative,innovative and dynamic Full Stack Engineer living in Kenya, Nairobi.', 'I love making things ', 'I build products and solutions using.^1000\n `JavaScript...` ^1000\nand `Ruby on Rails`', 'My current toolset includes AngularJS,Ember JS, Ruby on Rails and all the other various frameworks,libraries and technologies related to them', 'Feel free to ask me any questions.', 'I can help you in your project in all from the UI mockups,^1000\n ` back-end and front-end web development to fixing the design ` ^1000\n `and installing & configuration of the application on staging/production enviroments. `'],
+      fadeOut: true,
       typeSpeed: 40,
       backSpeed: 0,
       loop: true
@@ -7,11 +8,12 @@
 
 
   var typed2 = new Typed('.element2', {
-      strings: ['<span style="color:white !important">HI!</span>', 'I\nm', '<span>Philip Mutua</span>'],
+      strings: ['<span style="color:white !important">HI!</span>', 'I&rsquo;m', '<span>Philip Mutua</span>'],
       typeSpeed: 0,
       backSpeed: 0,
       fadeOut: true,
-      loop: true //change loop text
+      loop: true, //change loop text
+      loopCount: 2
   });
 
 
