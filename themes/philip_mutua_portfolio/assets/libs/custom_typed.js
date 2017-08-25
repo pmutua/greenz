53f0264d9f4d5bd906ac843530943cedb5f4c94f
pmutua <pmutua@live.com>
2017-08-25 12:21:48 +0300

    update css

--- themes/philip_mutua_portfolio/assets/libs/custom_typed.js
+++ themes/philip_mutua_portfolio/assets/libs/custom_typed.js
@@ -1,8 +1,8 @@
   var typed6 = new Typed('.element', {
-      strings: [' Hi,My name is Philip Mutua I am a creative,innovative and dynamic Full Stack Engineer living in Kenya, Nairobi.', 'I love making things ', 'I build products and solutions using.^1000\n `JavaScript...` ^1000\nand `Ruby on Rails`', 'My current toolset includes AngularJS,Ember JS, Ruby on Rails and all the other various frameworks,libraries and technologies related to them', 'Feel free to ask me any questions.', 'I can help you in your project in all from the UI mockups,^1000\n ` back-end and front-end web development to fixing the design ` ^1000\n `and installing & configuration of the application on staging/production enviroments. `'],
-      fadeOut: true,
+      strings: [' Hi,My name is Philip Mutua I am a creative,innovative and dynamic Full Stack Engineer living in Kenya, Nairobi.', 'I love making things ', 'I build products and solutions using.^1000\n `JavaScript...` ^1000\nand `Ruby on Rails`', 'My current toolset includes AngularJS,Ember JS, Ruby on Rails and all the other various frameworks,libraries and technologies related to them', 'I can help you in your project in all from ,^1000\n `UI mockups`,^1000\n `back-end` ,^1000\n`front-end` ^1000\nWeb development', 'installing & configuration of the app on staging/production enviroments.', 'Feel free to ask me any questions.'],
       typeSpeed: 40,
       backSpeed: 0,
+      fadeOut: true,
       loop: true
   });
 
