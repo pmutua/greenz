0b438622b17169f418ed9f258923ae9fe5493562
pmutua <pmutua@live.com>
2017-09-12 08:43:38 +0300

    update UI

--- themes/philip_mutua_portfolio/assets/libs/custom_typed.js
+++ themes/philip_mutua_portfolio/assets/libs/custom_typed.js
@@ -1,14 +1,14 @@
   var typed6 = new Typed('.element', {
-      strings: ['Hi,My name is Philip Mutua I am a creative,innovative and dynamic Full Stack Developer living in Kenya, Nairobi.',
-          'I love making things and understanding how they work. ',
-          ` I have experience developing and designing software for the web, from simple landing pages to progressive web applications. I strive to create software that not only functions efficiently under the hood, but also provides intuitive, pixel-perfect user experiences.`,
-          `I love learning new and better ways to create seamless user experiences with clean, efficient, and scalable code. I consider work an ongoing education, and I'm always looking for opportunities to work with those who are willing to share their knowledge as much as I want to learn. At the end of the day, my primary goal is to create something beautiful with people that bring out the best in me.`,
+      strings: ['Hi,I\nm Philip.I am a creative,innovative and dynamic Full Stack Developer living in Kenya, Nairobi.',
+          'I specialize and work with both front-end and backend technnologies.',
           '<ul>My current toolset that I use to create products and solutions includes: ^1000\n `<li>Angular 2 JS</li>`^1000\n `<li>Ember JS</li>` ^1000\n`<li>Ruby on Rails stack</li>`</ul>',
           'As a web developer,I am comfortable working with both Back end and Front-end technologies and I enjoy using my unequivocal love for making things, and my mission-driven work ethic to literally make things happen.',
 
+          ` I have experience developing and designing software for the web, from simple landing pages to progressive web applications. I strive to create software that not only functions efficiently under the hood, but also provides intuitive, pixel-perfect user experiences.`,
+          `I love learning new and better ways to create seamless user experiences with clean, efficient, and scalable code. I consider work an ongoing education, and I'm always looking for opportunities to work with those who are willing to share their knowledge as much as I want to learn. At the end of the day, my primary goal is to create something beautiful with people that bring out the best in me.`,
 
-          'I teach people too!<hr>^3000\n`I provide workshops on social media, blogging and using Medium. I’ve had some great feedback. `',
-          '',
+
+
           'When I\nm not in front of a computer screen, I\nm probably snowboarding, traveling, petting dogs, or learning a new song on my uke.and travel whenever I can.',
           'Feel free to ask me any questions.', 'I can help you in your project in all from ,^1000\n `UI mockups`,^1000\n `Front end` ,^1000\n`Back end`',
           'There are really no limits in terms of functionality, so whatever you want to be online, we can work together to make that happen.',
