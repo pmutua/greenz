ef85d38ea97ca36a3f0c10346b1ea964754f6c94
pmutua <pmutua@live.com>
2017-09-18 16:58:37 +0300

    added mockup assets

--- themes/philip_mutua_portfolio/assets/libs/custom_typed.js
+++ themes/philip_mutua_portfolio/assets/libs/custom_typed.js
@@ -1,16 +1,16 @@
   var typed6 = new Typed('.element', {
-      strings: ['Hi,I\nm Philip.I am a creative,innovative and dynamic Full Stack Developer living in Kenya, Nairobi.',
-          'I specialize and work with both front-end and backend technnologies.',
-          '<ul>My current toolset that I use to create products and solutions includes: ^1000\n `<li>Angular 2 JS</li>`^1000\n `<li>Ember JS</li>` ^1000\n`<li>Ruby on Rails stack</li>`</ul>',
-          'As a web developer,I am comfortable working with both Back end and Front-end technologies and I enjoy using my unequivocal love for making things, and my mission-driven work ethic to literally make things happen.',
+      strings: ['Hi my name is Philip Mutua and I\nm a Kenyan based full-stack developer passionate about making things with technology.',
+          ' I specialize in front-end development, responsive design, back-end development and hybrid apps.',
+          'I see tech as a tool for creating change to build the kind of world I want to live in.',
+          'When I\nm not in front of a computer screen, I\n probably watch my favourite soccer team play a match,I also like science, travelling and films.',
 
-          ` I have experience developing and designing software for the web, from simple landing pages to progressive web applications. I strive to create software that not only functions efficiently under the hood, but also provides intuitive, pixel-perfect user experiences.`,
+          `Well,now you know a little about me and what I can do.Have a look around.`,
           `I love learning new and better ways to create seamless user experiences with clean, efficient, and scalable code. I consider work an ongoing education, and I'm always looking for opportunities to work with those who are willing to share their knowledge as much as I want to learn. At the end of the day, my primary goal is to create something beautiful with people that bring out the best in me.`,
 
 
 
-          'When I\nm not in front of a computer screen, I\nm probably snowboarding, traveling, petting dogs, or learning a new song on my uke.and travel whenever I can.',
-          'Feel free to ask me any questions.', 'I can help you in your project in all from ,^1000\n `UI mockups`,^1000\n `Front end` ,^1000\n`Back end`',
+          'If you see something you like, please feel free to reach out! ',
+          'I\nm always on the prowl for the next exciting web development project. Enjoy! ',
           'There are really no limits in terms of functionality, so whatever you want to be online, we can work together to make that happen.',
 
       ],
@@ -29,7 +29,7 @@
       backSpeed: 0,
       fadeOut: true,
       loop: true, //change loop text
-      loopCount: 2
+      loopCount: 1
   });
 
 
